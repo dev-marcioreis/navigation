bb6d908c057da170d4b7d14c43587fe4e44aab95
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-05-02 11:03:58 -0300

    Dropdown js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,33 @@ navToggle.addEventListener('click', () =>{
     navMenu.classList.toggle('show-menu')
     toggleBtn.classList.toggle('show-icon')
 })
+
+const dropdownItems = document.querySelectorAll('.dropdown-item')
+
+dropdownItems.forEach((item) => {
+    const dropdownButton = item.querySelector('.dropdown-button')
+
+    dropdownButton.addEventListener('click', () => {
+
+        const showDropdown = document.querySelector('.show-dropdown')
+
+        toggleItem(item)
+
+        if(showDropdown && showDropdown!= item) {
+            toggleItem(showDropdown)
+        }
+
+    })
+})
+
+const toggleItem = item => {
+    const dropdownContainer = item.querySelector('.dropdown-container')
+
+    if(item.classList.contains('show-dropdown')) {
+        dropdownContainer.removeAttribute('style')
+            item.classList.remove('show-dropdown')
+    } else {
+        dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
+            item.classList.add('show-dropdown')
+    }  
+}
